be8aef49f84746dff440d1f13ff7c76e11cd2d59
Khanh Nguyen <Khanh13Nguyen13@gmail.com>
2024-03-25 23:04:21 +0100

    feat(page): added sketches

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,113 +1,80 @@
-import Image from "next/image";
-
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+	return (
+		<main>
+			<div className="w-[78%] m-auto">
+				<div>
+					<div>save & edit buttons</div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+					<div className="border-2 border-slate-600 border-solid h-full my-4 p-4">
+						<header className="header-block">
+							<div className="header-top">
+								<strong>Khanh Nguyen</strong>
+								<div>
+									<p>Kalchstraße 15</p>
+									<p>87700 Memmingen</p>
+									<p>+49 (0) 160 9343 7907</p>
+									<p>khanh13nguyen13@gmail.com</p>
+								</div>
+							</div>
+							<div className="header-bottom">
+								<div>
+									<p>Mothership GmbH</p>
+									<p>Andreas Emer</p>
+									<p>80636 Munich</p>
+								</div>
+								<p>Memmingen, 25. März 2024</p>
+								<br />
+							</div>
+						</header>
+						<section>
+							<strong>Frontend Webentwickler</strong>
+							<h1>Dear Andreas Emer</h1>
+							<br />
+							<article>
+								Last year, I had the pleasure of attending a conference at your
+								office dedicated to website performance and tools such as Google
+								Page Speed by Niklas Wolf. I was deeply impressed not only by
+								the atmosphere in your office but also by your approach to
+								development and the professionalism of your team. Your company
+								has left a lasting impression on me to this day. By sheer
+								chance, I stumbled upon a vacancy on the germantechjobs.de
+								website. I never thought such an opportunity would come by so
+								rarely, and I would like to briefly introduce myself and explain
+								how I can be of value to your team. My name is Khanh. Two years
+								ago, I relocated to Germany from Ukraine and have since been
+								actively seeking opportunities to contribute to a company I will
+								collaborate. In Ukraine, I specialized in developing various
+								types of websites, ranging from landing pages to E-commerce
+								platforms, with a strong focus on mobile responsiveness, loading
+								speed optimization, GPS integration, and enhancing user
+								experience. My skill set encompasses proficiency in HTML & CSS
+								(including SASS), jQuery, JavaScript, CSS animations, Bootstrap,
+								and GULP. Furthermore, I have recently expanded my expertise to
+								include emerging technologies such as React, TypeScript,
+								Next.js, and Node.js (Express), as evidenced by the five
+								completed projects showcased in my portfolio. I am a determined
+								and dedicated professional, driven by the desire to make a
+								meaningful contribution to a company. While my current
+								experience and skills may appear insufficient for your position,
+								my eagerness to learn, diligence, and genuine interest in new
+								technologies will enable me to quickly adapt and become a
+								valuable asset to your team.
+								<br />
+								<p>
+									Please feel free to email me via email at
+									Khanh13nguyen13@gmal.com with any further questions.
+								</p>
+								<p>Looking forward to your response.</p>
+								<p>Warm regards,</p>
+								<p className="font-bold">IMAGE</p>
+								<p>Khanh Nguyen</p>
+							</article>
+						</section>
+					</div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+					<div>button section</div>
+				</div>
+			</div>
+		</main>
+	);
 }
